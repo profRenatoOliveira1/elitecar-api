b58804ff085dad06a8accddeaa28f43e87376afe
Renato Oliveira <renato@MacBook-Air-de-Renato.local>
2025-10-21 15:22:38 -0300

    Criando conexão com o banco de dados e classe Cliente

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import { server } from "./server.js"; // importa o servidor HTTP
+import { DatabaseModel } from "./model/DatabaseModel.js";
 
 const port: number = 3333; // define a porta que o servidor vai executar
 
 // liga o servidor HTTP
-server.listen(port, () => {
-    console.log(`Servidor executando no endereço http://localhost:${port}`);
-});
+new DatabaseModel().testeConexao().then((resbd) => {
+    if(resbd) {
+        server.listen(port, () => {
+            console.log(`Servidor rodando em http://localhost:${port}`);
+        })
+    } else {
+        console.log('Não foi possível conectar ao banco de dados');
+    }
+})
